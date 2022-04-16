c5ab5dba93c2eaddd1bdb883f534b34a06efe6be
qaseemme064 <qaseem_nedian@hotmail.com>
2022-04-16 11:02:56 -0400

    t-test for suspension coils lot wise for 1500PSI

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -20,10 +20,17 @@ total_summary <- suspensionCoil %>% summarize(Mean=mean(PSI), Median=median(PSI)
 #calculation mean, median, mode, variance & SD by LOTS
 lot_summary <- suspensionCoil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), Stand.Dev=sd(PSI), .groups= 'keep')
 
+####Deliverable # 3
+t.test(suspensionCoil$PSI, mu=1500)
 
+#t-test for each lot
+lot1 <- subset(suspensionCoil, Manufacturing_Lot == 'Lot1')
+lot2 <- subset(suspensionCoil, Manufacturing_Lot == 'Lot2')
+lot3 <- subset(suspensionCoil, Manufacturing_Lot == 'Lot3')
 
-
-
+t.test(lot1$PSI, mu=1500)
+t.test(lot2$PSI, mu=1500)
+t.test(lot3$PSI, mu=1500)
 
 
 
