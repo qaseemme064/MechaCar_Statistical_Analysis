a03c57a507920175daea41a06dac71646dae025e
qaseemme064 <qaseem_nedian@hotmail.com>
2022-04-16 08:50:23 -0400

    suspension coil total and lot summary

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -6,5 +6,32 @@ MechaCar_mpg <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors 
 #passing linear regression in all six columns
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data= MechaCar_mpg)
 
-#simmary of linear regression
+#summary of linear regression
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data= MechaCar_mpg))
+
+
+#### Deliverable # 2
+
+suspensionCoil <- read.csv(file= 'Suspension_Coil.csv', check.names = F, stringsAsFactors = F)
+
+#calculation mean, median, mode, variance & SD
+total_summary <- suspensionCoil %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), Stand.Dev=sd(PSI), .groups= 'keep')
+
+#calculation mean, median, mode, variance & SD by LOTS
+lot_summary <- suspensionCoil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), Stand.Dev=sd(PSI), .groups= 'keep')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
